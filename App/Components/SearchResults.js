5859abb73a1a719078afd1c6406580308cc90eae
ses10 <sesma@live.com>
2016-03-05 13:45:30 -0500

    create basic listView with dummy data

--- App/Components/SearchResults.js
+++ App/Components/SearchResults.js
@@ -7,16 +7,39 @@ import React, {
   View,
   TextInput,
   TouchableHighlight,
+  ListView,
 } from 'react-native';
 
 class SearchResults extends Component {
+
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows([{name: 'Bob'},{name: 'Rob'}]),
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.resultText}>
-          Results
-        </Text>
+      <View style={styles.container}>        
+        <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
+
       </View>        
+    );
+  }
+
+  //for single row
+  renderRow(rowDats){
+    return(
+      <View style={styles.rowContainer}>
+        <Text style={styles.recipeName}> {rowDats.name} </Text>
+      </View>
     );
   }
 }
@@ -32,6 +55,27 @@ const styles = StyleSheet.create({
   resultText:{
     fontSize: 20,
     marginTop: 100,
+  },
+  listView: {
+    alignSelf: 'stretch',
+    padding: 5
+  },
+  rowContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    marginTop: 2,
+    marginBottom: 2,
+    borderRadius: 4,
+    shadowOpacity: .5,
+    shadowOffset:{height:2},
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  recipeName: {
+    alignSelf: 'center',
+
   }
 });
 
