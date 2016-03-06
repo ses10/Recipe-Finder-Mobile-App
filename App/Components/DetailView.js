a308c519162fb447a26a2dcc11868d351935bff4
ses10 <sesma@live.com>
2016-03-06 14:36:41 -0500

    add dummy data for

--- App/Components/DetailView.js
+++ App/Components/DetailView.js
@@ -10,31 +10,79 @@ import React, {
   TextInput,
   TouchableHighlight,
   ListView,
+  ScrollView,
+  Image,
 } from 'react-native';
 
 class DetailView extends Component {
+
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: dataSource.cloneWithRows([{name: 'Chicken Fingers', url: 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2008/3/28/0/NY0108_Chicken-Tenders-with-Honey-Mustard-Sauce.jpg.rend.sni12col.landscape.jpeg'}]),
+    };
+ 
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.resultText}>
-          DetailView
-        </Text>
-      </View>        
+        <ListView
+          style = {styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRecipe}
+        />
+      </View> 
     );
+  }
+
+  renderRecipe(recipeData){
+    return(
+        <View style={styles.recipeView}>
+          <Text style={styles.recipeTitle}>
+            {recipeData.name}
+          </Text>
+          <Image
+            style={styles.image}
+            source={{uri: recipeData.url}}
+          />
+        </View>
+      );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F8F8F8',
     paddingLeft: 30,
     paddingRight: 30,
+    paddingTop: 0,
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center',
   },
-  resultText:{
-    fontSize: 20,
-    marginTop: 100,
+  listView: {
+    //backgroundColor: 'black',
+    alignSelf: 'stretch',
+    padding: 5,
+  },
+  recipeView:{
+    backgroundColor: '#FFF',
+    marginTop: 2,
+    borderRadius: 4,
+    shadowOpacity: .5,
+    shadowOffset:{height:2},
+    alignItems: 'center',
+    paddingBottom: 5,
+  },
+  recipeTitle:{
+    fontSize: 18,
+    margin: 5,
+  },
+  image:{
+    width: 290,
+    height: 180,
   }
 });
 
