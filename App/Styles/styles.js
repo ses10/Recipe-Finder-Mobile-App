44dbb363e42929b7f69b9f100e880588d3e92a6c
ses10 <sesma@live.com>
2016-03-26 11:52:41 -0400

    add global styles

---
+++ App/Styles/styles.js
@@ -0,0 +1,22 @@
+'use strict';
+var React = require('react-native');
+var {
+  StyleSheet,
+} = React;
+
+module.exports = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F8F8F8',
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
+
+});
+
+
+
+
+
